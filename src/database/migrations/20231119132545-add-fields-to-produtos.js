aa1c11871f041bc49d24fed90bc9eec257301889
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2023-11-19 11:04:10 -0300

    ajuste migration produtos 3

--- src/database/migrations/20231119132545-add-fields-to-produtos.js
+++ src/database/migrations/20231119132545-add-fields-to-produtos.js
@@ -2,32 +2,34 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.addColumn('saida', 'produtos', {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
-    queryInterface.addColumn('retorno', 'produtos', {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
-    queryInterface.addColumn('valoravista', 'produtos', {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
-    queryInterface.addColumn('valoraprazo', 'produtos', {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
-    queryInterface.addColumn('parcelas', 'produtos', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    });
-    queryInterface.removeColumn('marca', 'produtos');
-    queryInterface.removeColumn('embalagem', 'produtos');
-    queryInterface.removeColumn('peso', 'produtos');
-    queryInterface.removeColumn('informacaoalergica', 'produtos');
-    queryInterface.removeColumn('garantia', 'produtos');
-    return queryInterface.removeColumn('paisorigem', 'produtos');
+    return [
+      queryInterface.addColumn('saida', 'produtos', {
+        type: Sequelize.DATE,
+        allowNull: true,
+      }),
+      queryInterface.addColumn('retorno', 'produtos', {
+        type: Sequelize.DATE,
+        allowNull: true,
+      }),
+      queryInterface.addColumn('valoravista', 'produtos', {
+        type: Sequelize.DATE,
+        allowNull: true,
+      }),
+      queryInterface.addColumn('valoraprazo', 'produtos', {
+        type: Sequelize.DATE,
+        allowNull: true,
+      }),
+      queryInterface.addColumn('parcelas', 'produtos', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      }),
+      queryInterface.removeColumn('marca', 'produtos'),
+      queryInterface.removeColumn('embalagem', 'produtos'),
+      queryInterface.removeColumn('peso', 'produtos'),
+      queryInterface.removeColumn('informacaoalergica', 'produtos'),
+      queryInterface.removeColumn('garantia', 'produtos'),
+      queryInterface.removeColumn('paisorigem', 'produtos'),
+    ];
   },
 
   down: (queryInterface, Sequelize) => {
