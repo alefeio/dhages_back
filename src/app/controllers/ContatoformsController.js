eab3a721b3d36580dbcc0b432f242d215f30b45a
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2023-11-26 03:50:13 -0300

    delete contatos mensagem - ajuste 4

--- src/app/controllers/ContatoformsController.js
+++ src/app/controllers/ContatoformsController.js
@@ -61,15 +61,7 @@ class ContatoformsController {
 
     const contatos = await Contatoforms.findAll({
       where: { lida: false },
-      order: [['created_at', 'DESC']],
-      attributes: [
-        'nome',
-        'email',
-        'telefone',
-        'assunto',
-        'mensagem',
-        'created_at',
-      ],
+      order: [['created_at', 'DESC']]
     });
 
     return res.json(contatos);
