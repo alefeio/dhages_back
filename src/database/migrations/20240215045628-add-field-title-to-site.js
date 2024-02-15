aa0be45cb1a878a62694f6c9d7d2d11769d18848
alefeio@gmail.com <alefeio@gmail.com>
2024-02-15 02:23:52 -0300

    add title in site - ajust 2

--- src/database/migrations/20240215045628-add-field-title-to-site.js
+++ src/database/migrations/20240215045628-add-field-title-to-site.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('sites', 'title', {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
     });
   },
 
